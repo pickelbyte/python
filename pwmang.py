77f1a44e1598a16e4ee3579738c8eb374defd337
pickelbyte <122392470+pickelbyte@users.noreply.github.com>
2023-01-19 09:22:07 -0800

    Update pwmang.py

--- pwmang.py
+++ pwmang.py
@@ -15,24 +15,27 @@ root.title("PWMang")
 root.configure(bg="#707070")
 root.resizable(False, False)
 
-def read_csv(file='C:/Users/s-luklee/Documents/passtemplate.csv'):
-    try:
-        with open(file, newline='') as csvfile:
-            contents = list(csv.reader(csvfile, delimiter=","))
-        return contents
-    except PermissionError:
-        pass
+def invalid_popup():
+    popup = tk.Toplevel(win)
+    top.geometry("200x100")
+    top.title("Warning")
+    Label(top, text= """Invalid File!
+                        CSV required""", font=Mfont).place(x=150,y=80)
 
 def browse_files():
     filename = filedialog.askopenfilename(initialdir = main_folder, title = "Select a File", filetypes = (("CSV files", "*.csv*"),("all files","*.*")))
     root.title(f"PWMang - {filename}")
-    return(filename)
+    with open(filename, newline='') as csvfile:
+        contents = list(csv.reader(csvfile, delimiter=","))
+    return contents
+    
 
 pwlist = tk.Frame(root, width=480, height=460, bg="#b5b5b5")
 pwlist.place(x=230, y=10)
 sidebar = tk.Frame(root, width=200, height=460, bg="#b5b5b5")
 sidebar.place(x=10, y=10)
 sidebar.pack_propagate(False)
+pwlist.pack_propagate(False)
 
 selectfilebtn = tk.Button(sidebar, text="Import Passwords", font=Mfont, command=browse_files)
 selectfilebtn.place(x=2, y=2)
